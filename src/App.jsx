b7f50a348a75aa5ee79cc56e91ff158f0e3fb450
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-04-09 01:39:29 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
 function App() {
-  const [data, setData] = useState([])
+  const handleLogin = () => {
+    window.location.href = "http://localhost:3000/auth/google";
+  };
+
+  const [token, setToken] = useState("");
+  useEffect(() => {
+    const tokenValue = Cookies.get("token");
+    if (tokenValue) {
+      setToken(tokenValue);
+    } else {
+      console.log("k co token");
+    }
+  }, []);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/')
-        setData(response.data)
+        const response = await axios.get(
+          "http://localhost:3000/Student/getAll",
+          {
+            headers: {
+              authorization: `"Bearer ${token}"`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setData(response.data);
+        console.log(response.data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-    
-    fetchData() 
-  }, []) 
+    };
 
+    fetchData();
+  }, [token]);
   return (
-    <>
-      <h1>Test Data gửi qua frontend</h1>
-      {data.map(user => (
-        <div key={user.user_id}>
-          <h2>{user.full_name}</h2>
+    <div className="App">
+      <h1>Login with Google</h1>
+
+      <button onClick={handleLogin}>Login with Google</button>
+      {data.map((Student) => (
+        <div key={Student.MaSV}>
+          <h2>{Student.St_Fullname}</h2>
         </div>
       ))}
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
